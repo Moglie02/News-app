cd3e3a1adfcce0d42818d0724337ff1089dee3e0
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-03 22:47:13 +0300

    added python version 3.10

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-from main import create_app
-from flask_script import Manager,Server
+from app import create_app
+from flask import Manager,Server
 
 # Creating app instance
 app = create_app('development')
