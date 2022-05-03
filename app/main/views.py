c8a5c98de5ea6f0f9591acd2b6de50bf2e99d610
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-03 22:28:07 +0300

    EDIT

--- app/main/views.py
+++ app/main/views.py
@@ -10,7 +10,7 @@ def index():
     View root page function that returns the index page and its data
     '''
     news = get_news() 
-    title = 'KIMNEWS'
+    title = 'GALAXYNEWS'
     return render_template('index.html', title = title,articles = news)
 @main.route('/sources')
 def sources():
