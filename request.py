70a3556e40d2e42365e90b0c1b4ec44f0f993a14
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-03 19:06:27 +0300

    added functions,responsive to my api

--- request.py
+++ request.py
@@ -1,3 +1,4 @@
+from tkinter import NONE
 import urllib.request,json
 from .model import News
 #getting api key
@@ -15,4 +16,30 @@ def configure_request(app):
         '''
         get_news_url = base_url.format(api_key)
 
-        
+        with urllib.request.urlopen(get_news_url)as url:
+            get_news_data = url.read()
+            get_news_response = json.loads(get_news_data)
+
+            news_results =NONE
+
+            if get_news_response['articles']:
+                news_results_list = get_news_response['articles']
+
+                #print(news_results_list)
+                news_results=process_results(news_results_list)
+
+                return news_results
+                def process_results(news_list):
+
+                    news_results =[]
+                    for news_item in news_list:
+                        title = news_item.get('title')
+                        description = news_item.get('description')
+                        urlToImage = news_item.get('urlToImage')
+                        content = news_item.get('content')
+                        publishedAt = news_item.get('publishedAt')
+
+                        news_object = News(title,description,urlToImage,content,publishedAt)
+                        news_results.append(news_object)
+
+                        return news_results
