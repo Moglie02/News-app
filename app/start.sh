81d81da4dd362b85fa25121de7ca7bc25780a37a
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-04 19:01:13 +0300

    export news api

---
+++ app/start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='8582da6ea01543449ee429fa3e7e01cc'
+
+
+python3.6 manage.py server
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?country=us&apiKey=8582da6ea01543449ee429fa3e7e01cc'
-    NEWS_API_KEY = os.environ.get('8582da6ea01543449ee429fa3e7e01cc')
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 
